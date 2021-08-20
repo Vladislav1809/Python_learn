623a51307b87126cc9a10097b6ba6963885b4f99
Vladislav1809 <85053033+Vladislav1809@users.noreply.github.com>
2021-08-20 17:19:29 +0300

    Add files via upload
    h/w on lesson 5

--- 19.08.2021_1-2.py
+++ 19.08.2021_1-2.py
@@ -1,1 +1,16 @@
+# Number_1
+"""
+def odd_nums(finish_number):
+    x = 1
+    while x <= finish_number:
+        yield x
+        x += 2
 
+
+odd_to_15 = odd_nums(15)
+"""
+# Number_2
+finish_number = 15
+odd_nums_generator = (x for x in range(1, finish_number + 1, 2))
+print(next(odd_nums_generator))
+# print(next(odd_nums_generator))
